f48aadf3d1c6759b8f704f7232abee0f851a8a53
Taqi Raza <mttalpur@gmail.com>
2019-06-30 00:12:13 +0500

    Revert "sigh...."
    This reverts commit 10897f0deaa99071885deffc8259ffa20b59725c.

--- deployscript/bionic-wine-staging-lol-deploy.sh
+++ deployscript/bionic-wine-staging-lol-deploy.sh
@@ -9,10 +9,10 @@ wget -nvc https://gist.github.com/mmtrt/578f4c0694fcfc968b2d9dcc90da4c0e/raw/9bc
 wget -nvc https://gist.github.com/mmtrt/578f4c0694fcfc968b2d9dcc90da4c0e/raw/9bcb0abfede983a7093973d33f206da9023a2980/wine-staging-i386_4.10~bionic_i386.deb -O wine-staging-i386_4.10-lol~bionic_i386.deb
 wget -nvc https://gist.github.com/mmtrt/578f4c0694fcfc968b2d9dcc90da4c0e/raw/9bcb0abfede983a7093973d33f206da9023a2980/libc6_2.27-3ubuntu1_i386.deb
 
-dpkg -x "wine-staging_4.10-lol~bionic_i386.deb" wineversion/
-dpkg -x "wine-staging-i386_4.10-lol~bionic_i386.deb" wineversion/
+dpkg -x wine-staging_4.10-lol~bionic_i386.deb wineversion/
+dpkg -x wine-staging-i386_4.10-lol~bionic_i386.deb wineversion/
 
-cp -r "wineversion/opt/"* "wineversion"
+cp -r "wineversion/opt/wine-staging/"* "wineversion"
 rm -r "wineversion/opt"
 rm -rf "wineversion/usr"
 
